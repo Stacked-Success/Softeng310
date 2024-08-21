8032410141b7b6c170680d313abb34b6da56de2e
axu732 <axu732@aucklanduni.ac.nz>
2024-08-21 23:29:22 +1200

    Dummy commit

--- src/main/java/com/stackedsuccess/Main.java
+++ src/main/java/com/stackedsuccess/Main.java
@@ -9,7 +9,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-  private static Scene scene;
+  public static Scene scene;
 
   public static void main(String[] args) {
     launch();
