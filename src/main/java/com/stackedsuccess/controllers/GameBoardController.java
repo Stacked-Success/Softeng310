484d11dc4ae852631f906d27c3c3f1ddb2a9b079
axu732 <axu732@aucklanduni.ac.nz>
2024-08-22 00:36:36 +1200

    Added flow back to the main screen

--- src/main/java/com/stackedsuccess/controllers/GameBoardController.java
+++ src/main/java/com/stackedsuccess/controllers/GameBoardController.java
@@ -1,6 +1,9 @@
 package com.stackedsuccess.controllers;
 
 import com.stackedsuccess.GameInstance;
+import com.stackedsuccess.Main;
+import com.stackedsuccess.SceneManager;
+import com.stackedsuccess.SceneManager.AppUI;
 import com.stackedsuccess.ScoreRecorder;
 import com.stackedsuccess.tetriminos.*;
 import java.io.IOException;
@@ -10,6 +13,8 @@ import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.ColorAdjust;
@@ -136,8 +141,9 @@ public class GameBoardController {
   }
 
   @FXML
-  void onClickRestart(ActionEvent event) {
-    // will add functionality once main menu is made
+  void onClickRestart(ActionEvent event) throws IOException {
+    SceneManager.addScene(AppUI.MAIN_MENU, loadFxml("HomeScreen"));
+    Main.setUi(AppUI.MAIN_MENU);
   }
 
   /**
@@ -396,5 +402,16 @@ public class GameBoardController {
    */
   private Stage getStage() {
     return (Stage) basePane.getScene().getWindow();
+  }
+
+  /**
+   * Loads the FXML file and returns the parent node.
+   *
+   * @param fxml the name of the FXML file (without extension)
+   * @return the parent node of the input file
+   * @throws IOException if the file is not found
+   */
+  public static Parent loadFxml(final String fxml) throws IOException {
+    return new FXMLLoader(Main.class.getResource("/fxml/" + fxml + ".fxml")).load();
   }
 }
