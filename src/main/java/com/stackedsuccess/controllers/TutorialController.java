c66db9a88222558a63961f18f932d5034623529f
manners99 <126521915+manners99@users.noreply.github.com>
2024-09-16 22:59:28 +1200

    added javadocs

--- src/main/java/com/stackedsuccess/controllers/TutorialController.java
+++ src/main/java/com/stackedsuccess/controllers/TutorialController.java
@@ -9,23 +9,42 @@ public class TutorialController {
     private ImageView exitBtb;
 
     public void initialize() {
+        //Utilize the deafault constructor build into javafx
     }
 
+    /**
+     * This method expands the size of the exit cross when it is hovered
+     * over to help users recognise that it is a clickable object
+     * 
+     * @param event the action of the user hovering their mouse over the exit cross
+     */
     @FXML
     private void onExitHoveredOver(MouseEvent event) {
         exitBtb.scaleXProperty().set(1.2);
         exitBtb.scaleYProperty().set(1.2);
     }
 
+    /**
+     * Retruns the size of the exit cross to its original size when the user
+     * is no longer hovering over it
+     * 
+     * @param event the users mouse being moved away from the exit cross
+     */
     @FXML
     private void onExitNotHoveredOver(MouseEvent event) {
         exitBtb.scaleXProperty().set(1);
         exitBtb.scaleYProperty().set(1);
     }
 
+    /**
+     * This method closes the tutorial screen and returns the user to their game
+     * when the user clicks on the exit cross
+     * 
+     * @param event the action of clicking the exit xross
+     */
     @FXML
     private void onExitClicked(MouseEvent event) {
-        System.exit(0);
+        //TODO: Add code to close the tutorial screen
     }
     
 }
